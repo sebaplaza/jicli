446dd9d1f9c2084ee1695002d61acaf5a3b895ac
Sebastian Plaza <sebastianplaza@gmail.com>
2022-01-10 21:02:29 +0100

    refactor: organize main code

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use base64::encode;
 use clap::Parser;
 use reqwest::Client;
+use std::collections::HashMap;
 use std::env;
-use std::{collections::HashMap, error::Error};
+use std::error::Error;
 
 /// Simple JIRA client to add worklog time to an issue
-#[derive(Parser, Debug)]
+#[derive(Parser)]
 #[clap(about, version, author)]
 struct Args {
     /// key of jira issue
@@ -16,6 +17,47 @@ struct Args {
     #[clap(short, long)]
     time: String,
 }
+
+struct Jira {
+    url_base: String,
+    url_api: String,
+    user: String,
+    password: String,
+}
+
+impl Jira {
+    fn new(url: String, user: String, password: String) -> Jira {
+        Jira {
+            url_base: url.clone(),
+            url_api: format!("{}/rest/api/latest", url),
+            user,
+            password,
+        }
+    }
+    async fn call_api(&self, path: String, json_map: HashMap<&str, String>) {
+        let url = format!("{}{}", self.url_api, path);
+        let user_pass = format!("{}:{}", self.user, self.password);
+        let secret = encode(user_pass);
+        let client = Client::new();
+        let authorization = "Basic ".to_owned() + &secret;
+        client
+            .post(url)
+            .json(&json_map)
+            .header("Authorization", authorization)
+            .send()
+            .await
+            .expect("Problem calling the JIRA API");
+    }
+    async fn add_work_load(&self, issue: String, time: String) {
+        let mut json_map = HashMap::new();
+        json_map.insert("timeSpent", time.clone());
+        let path = format!("/issue/{}/worklog", issue);
+        self.call_api(path, json_map).await;
+        let issue_url = format!("{}/browse/{}", self.url_base, issue);
+        println!("'{}' has been added to issue {}", &time, &issue_url);
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
@@ -23,28 +65,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let jira_password =
         env::var("JIRA_PASSWORD").expect("Please set $JIRA_PASSWORD environnement variable");
     let jira_url = env::var("JIRA_URL").expect("Please set $JIRA_URL environnement variable");
-
-    let client = Client::new();
-
-    let secret = encode([jira_user, jira_password].join(":"));
-    // let secret = encode(format!("{}:{}", args.user, args.password));
-    // let secret = encode(args.user + ":" + &args.password);
-
-    let mut json_map = HashMap::new();
-    json_map.insert("timeSpent", args.time);
-
-    let url = format!("{}/rest/api/latest/issue/{}/worklog", jira_url, args.issue);
-    let res = client
-        .post(url)
-        .json(&json_map)
-        .header("Authorization", "Basic ".to_owned() + &secret)
-        .send()
-        .await?;
-    // Parse the response body as Json in this case
-    // let json = res.text().await?;
-    // println!("{:?}", json);
-
-    // println!("{} added to issue {}", args.time, args.issue);
-    println!("time added to issue");
+    let jira = Jira::new(jira_url, jira_user, jira_password);
+    jira.add_work_load(args.issue, args.time).await;
     Ok(())
 }
