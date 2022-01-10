cae6acbd250041a00f6494ccc2a1c90e93023385
Sebastian Plaza <sebastianplaza@gmail.com>
2022-01-10 21:41:51 +0100

    refactor: create jira module

---
+++ src/jira.rs
@@ -0,0 +1,56 @@
+use base64::encode;
+use reqwest::Client;
+use std::collections::HashMap;
+
+pub struct Jira {
+    url_base: String,
+    url_api: String,
+    user: String,
+    password: String,
+}
+
+impl Jira {
+    pub fn new(url: String, user: String, password: String) -> Jira {
+        Jira {
+            url_base: url.clone(),
+            url_api: format!("{}/rest/api/latest", url),
+            user,
+            password,
+        }
+    }
+    pub async fn call_api(&self, path: String, json_map: HashMap<&str, String>) -> (u16, String) {
+        let url = format!("{}{}", self.url_api, path);
+        let user_pass = format!("{}:{}", self.user, self.password);
+        let secret = encode(user_pass);
+        let client = Client::new();
+        let authorization = "Basic ".to_owned() + &secret;
+        let res = client
+            .post(url)
+            .json(&json_map)
+            .header("Authorization", authorization)
+            .send()
+            .await
+            .expect("Problem calling the JIRA API");
+        let status = res.status().as_u16();
+        let data = res
+            .text()
+            .await
+            .expect("Problem extracting data from API call");
+        return (status, data);
+    }
+    pub async fn add_work_load(&self, issue: String, time: String) {
+        let mut json_map = HashMap::new();
+        json_map.insert("timeSpent", time.clone());
+        let path = format!("/issue/{}/worklog", issue);
+        let (status, data) = self.call_api(path, json_map).await;
+        if status >= 400 {
+            eprintln!(
+                "statusCode '{}' when calling API / details: {}",
+                status, data
+            );
+            return;
+        }
+        let issue_url = format!("{}/browse/{}", self.url_base, issue);
+        println!("'{}' has been added to issue {}", &time, &issue_url);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
-use base64::encode;
 use clap::Parser;
-use reqwest::Client;
-use std::collections::HashMap;
 use std::env;
 use std::error::Error;
-
+mod jira;
 /// Simple JIRA client to add worklog time to an issue
 #[derive(Parser)]
 #[clap(about, version, author)]
@@ -18,46 +15,6 @@ struct Args {
     time: String,
 }
 
-struct Jira {
-    url_base: String,
-    url_api: String,
-    user: String,
-    password: String,
-}
-
-impl Jira {
-    fn new(url: String, user: String, password: String) -> Jira {
-        Jira {
-            url_base: url.clone(),
-            url_api: format!("{}/rest/api/latest", url),
-            user,
-            password,
-        }
-    }
-    async fn call_api(&self, path: String, json_map: HashMap<&str, String>) {
-        let url = format!("{}{}", self.url_api, path);
-        let user_pass = format!("{}:{}", self.user, self.password);
-        let secret = encode(user_pass);
-        let client = Client::new();
-        let authorization = "Basic ".to_owned() + &secret;
-        client
-            .post(url)
-            .json(&json_map)
-            .header("Authorization", authorization)
-            .send()
-            .await
-            .expect("Problem calling the JIRA API");
-    }
-    async fn add_work_load(&self, issue: String, time: String) {
-        let mut json_map = HashMap::new();
-        json_map.insert("timeSpent", time.clone());
-        let path = format!("/issue/{}/worklog", issue);
-        self.call_api(path, json_map).await;
-        let issue_url = format!("{}/browse/{}", self.url_base, issue);
-        println!("'{}' has been added to issue {}", &time, &issue_url);
-    }
-}
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
@@ -65,7 +22,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let jira_password =
         env::var("JIRA_PASSWORD").expect("Please set $JIRA_PASSWORD environnement variable");
     let jira_url = env::var("JIRA_URL").expect("Please set $JIRA_URL environnement variable");
-    let jira = Jira::new(jira_url, jira_user, jira_password);
-    jira.add_work_load(args.issue, args.time).await;
+    let jira_instance = jira::Jira::new(jira_url, jira_user, jira_password);
+    jira_instance.add_work_load(args.issue, args.time).await;
     Ok(())
 }
